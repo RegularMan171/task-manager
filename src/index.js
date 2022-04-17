f4343a836e5d0ab7eeded5e082e4bc02298db18a
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-04-17 23:24:10 +0530

    created separate user route file

--- src/index.js
+++ src/index.js
@@ -3,103 +3,14 @@ require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
 const ObjectId = require('mongodb').ObjectId
+const userRouter = require('./routers/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 //automatically parses req to json object
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch(e) {
-        res.status(400).send(e)
-    }
-})
-
-app.get('/users', async (req, res) => {
-
-    try {
-        const users = await User.find({})
-        res.status(200).send(users)
-    } catch(e) {
-        res.status(500).send()
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id)
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch(e) {
-        res.status(500).send(e)
-    }
-
-    // const id = new ObjectId(_id)
-
-    // User.findById(id).then((user) => {
-    //     console.log('user', user, id)
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-        
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
-
-app.patch('/users/:id', async (req, res) => {
-    
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
-    
-    if(!isValidOperation) {
-        return res.status(400).send({error: 'Invalid updates' })
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch(e) {
-        res.status(400).send()
-    }
-})
-
-app.delete('/users/:id', async(req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user)
-    } catch(e) {
-        res.status(500).send(e)
-    }
-})
-
-app.delete('/tasks/:id', async(req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-        if(!task) {
-            return res.status(404).send()
-        }
-        res.status(200).send(task)
-    } catch(e) {
-        res.status(500).send(e)
-    }
-})
+app.use(userRouter)
 
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
@@ -143,8 +54,7 @@ app.patch('/tasks/:id', async (req, res) => {
     if(!isValidOperation) {
         return res.status(400).send({error: 'Invalid updates' })
     }
-
-
+    
     try {
         const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
         if(!task) {
@@ -167,7 +77,6 @@ app.delete('/tasks/:id', async(req, res) => {
         res.status(500).send(e)
     }
 })
-
 
 //start server
 app.listen(port, () => {
