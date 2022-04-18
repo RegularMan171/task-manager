cb5a6f2c62c39e764467e0478797feaaa32d7150
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-04-18 21:44:50 +0530

    authentication for all the user routes

--- src/routers/user.js
+++ src/routers/user.js
@@ -67,6 +67,53 @@ router.post('/users/logoutall', auth, async (req, res) => {
     }
 })
 
+router.patch('/users/me', auth, async (req, res) => {
+    
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+    
+    if(!isValidOperation) {
+        return res.status(400).send({error: 'Invalid updates' })
+    }
+
+    try {
+
+        //const user = await User.findById(req.params.id)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        res.send(req.user)
+    } catch(e) {
+        res.status(400).send()
+    }
+})
+
+//delete profile
+router.delete('/users/me', auth, async(req, res) => {
+    try {
+        await req.user.remove()
+        res.status(200).send(req.user)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+})
+
+//not to be used
+router.delete('/users/:id', async(req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.status(200).send(user)
+    } catch(e) {
+        res.status(500).send(e)
+    }
+})
+
+//not to be used
 router.get('/users/:id', async (req, res) => {
     const _id = req.params.id
     try {
@@ -93,6 +140,7 @@ router.get('/users/:id', async (req, res) => {
     // })
 })
 
+//not to be used
 router.patch('/users/:id', async (req, res) => {
     
     const updates = Object.keys(req.body)
@@ -116,18 +164,6 @@ router.patch('/users/:id', async (req, res) => {
         res.send(user)
     } catch(e) {
         res.status(400).send()
-    }
-})
-
-router.delete('/users/:id', async(req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user)
-    } catch(e) {
-        res.status(500).send(e)
     }
 })
 
