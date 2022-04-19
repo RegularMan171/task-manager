c4e3c2fc0a4d92230be5f6b564821290f521ac1b
Sarath Konuru <sarath.konuru@hitachivantara.com>
2022-04-19 13:45:09 +0530

    added sharp to work on images and done with images

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,7 @@ const router = new express.Router()
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const multer = require('multer')
+const sharp = require('sharp')
 
 //sign up
 router.post('/users', async (req, res) => {
@@ -55,11 +56,14 @@ const upload = multer ({
         }
         cb(undefined, true)
     }
-
 })
+
 //upload profile pic
 router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
+    //req.user.avatar = req.file.buffer
     await req.user.save()    
     res.send({"message": "succesfully uploaded"})
 }, (error, req, res, next) => {
@@ -71,6 +75,21 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
     req.user.avatar = undefined
     await req.user.save()
     res.send()
+})
+
+//fetching avatar
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+        res.set('Content-Type', 'image/PNG')
+        res.send(user.avatar)
+
+    } catch(e) {
+        res.status(404).send()
+    }
 })
 
 //logout
